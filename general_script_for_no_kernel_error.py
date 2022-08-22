484766bb5ef60a7f63c6e209276b414fe2b6700c
htmos6 <88316097+htmos6@users.noreply.github.com>
2022-08-22 21:00:43 +0300

    Update general_script_for_no_kernel_error.py

--- general_script_for_no_kernel_error.py
+++ general_script_for_no_kernel_error.py
@@ -5,6 +5,7 @@ img = cv2.imread("path_to_opened_image")
 while (True):
     cv2.imshow("Puppy", img)
     
+    # if we have waited 1ms AND pressed ESC --> Exit
     if (cv2.waitKey(1) and 0xFF == 27):
        break
 cv2.destroyAllWindows()
